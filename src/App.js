e721c136e1e6fa64a183816e1a0b17806183e96b
aran <1903283729@qq.com>
2020-07-03 10:31:36 +0800

    feat: 001

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import logo from "./logo.svg"
+import "./App.css"
 
 function App() {
+  React.useEffect(() => {
+    const ele = document.getElementById('dd')
+    const observer = new IntersectionObserver((entries) => {
+      console.log(entries)
+    });
+    observer.observe(ele)
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="card" id='dd'>todo</div>
+      <div className="card">todo</div>
+      <div className="card">todo</div>
+      <div className="card">todo</div>
+      <div className="card">todo</div>
+      <div className="card">todo</div>
+      <div className="card" >todo</div>
+      <div className="freeze" id='sx'>我是个神仙</div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
